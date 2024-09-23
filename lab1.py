2b103cc84301e5525b7022e4a69426c58071202f
Deemehh <154468423+Deemehh@users.noreply.github.com>
2024-09-23 17:50:57 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,12 +1,14 @@
 ##### ЗАДАНИЕ 1
-num = int(input("введите число: "))
-for i in range(1, num + 1):
-    print(i)
+#num = int(input("введите число: "))
+#for i in range(1, num + 1):
+#    print(i)
 
 ##### ЗАДАНИЕ 2
-# num1 = int(input("введите первое число: "))
-# num2 = int(input("введите второе число: "))
-# if num1 > num2:
-#     print("наибольшее число:",num1)
-# else:
-#     print("наибольшее число:",num2)
+num1 = int(input("введите первое число: "))
+num2 = int(input("введите второе число: "))
+if num1 >= num2:
+     print("наибольшее число:",num1)
+elif num1 <= num2:
+     print("наибольшее число:",num2)
+else:
+     print("erm")
